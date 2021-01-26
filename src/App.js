ecfb2c57cdc59d5ae18c3043b83ab48a81e73f9d
CarolY <mon_alisa75@yahoo.com>
2021-01-26 00:15:13 -0800

    Added wrapper.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
+import Wrapper from "./components/Wrapper";
+
 import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Wrapper>
+        <MainContainer />
+      </Wrapper>
+    </>
+  );
 }
 
 export default App;
