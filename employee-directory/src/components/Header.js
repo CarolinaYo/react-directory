c73845d6432c9bd28814fdd3ddc54f6e79375939
CarolY <mon_alisa75@yahoo.com>
2021-01-25 23:29:12 -0800

    Add needed dependencies.

---
+++ employee-directory/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div
+      className="header"
+      style={{
+        color: "wheat",
+        backgroundColor: "#684551",
+        width: "100%",
+        textAlign: "center",
+        padding: "20px",
+        marginBottom: "5px",
+      }}
+    >
+      <h1>Employee Directory</h1>
+    </div>
+  );
+}
+
+export default Header;
