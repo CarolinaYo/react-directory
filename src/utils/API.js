48ea61b4643c94a84c86fb80b9d775e52d95ddd4
CarolY <mon_alisa75@yahoo.com>
2021-01-27 11:09:12 -0800

    Reformat code.

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/?results=";
-const limit = 100;
-
-// Export an object with a "search" method that searches the API for the passed query
 const API = {
-  searchRandomUser: function () {
-    return axios.get(BASEURL + limit);
+  getRandomUser: function () {
+    return axios.get("https://randomuser.me/api/?results=100");
   },
 };
 
