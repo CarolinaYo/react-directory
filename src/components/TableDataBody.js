3a23586a3feb98eeafc02988164d13b538dee71c
CarolY <mon_alisa75@yahoo.com>
2021-01-29 23:45:40 -0800

    Passed props from mainContainer and displayed data.

--- src/components/TableDataBody.js
+++ src/components/TableDataBody.js
@@ -1,15 +1,17 @@
 import React from "react";
-// import { Table } from "react-bootstrap";
 
-//Call the API and display data (picture, name, email and dobto rows.
+//Call the API and display data (picture, name, email and ph rows.
 function TableDataBody(props) {
-  console.log("Employees: ", props.employees);
+  // console.log("Employees: ", props.employees);
   let employeeRow = props.employees.map((employee, index) => {
     return (
-      <tr>
+      <tr key={index}>
         <td>{index + 1}</td>
         <td>
-          <img src={employee.picture.thumbnail} alt="employee photo" />
+          <img
+            src={employee.picture.thumbnail}
+            alt={"employee" + index + " photo"}
+          />
         </td>
         <td>{employee.name.first + " " + employee.name.last} </td>
         <td>{employee.email} </td>
